ae4520953ddc6f29afb35a2ff7f07a0178ac7d1c
DuAlexandre <luis.edu.alex@gmail.com>
2022-05-20 10:02:24 -0300

    Adiciona exercicios de for in e for off.

---
+++ fundamentos-do-desenvolvimento-web/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objeto-e-funcoes/aula-4-4.js
@@ -0,0 +1,56 @@
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: {
+        golden: 2,
+        silver: 3,
+    },
+};
+
+let message = "A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade."
+//console.log(message);
+
+player['bestInTheWorld'] = [2006, 2007,2008, 2009, 2010, 2018];
+
+//console.table(player);
+let best = player.bestInTheWorld;
+console.log(best);
+//console.log("A jogadora " + player.name + " " + player.lastName + " foi eleita a melhor do mundo por " + best.length + " vezes.");
+console.log("A jogadora possui " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + " medalhas de prata.")
+
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+for (let index in cars) {
+  console.log(cars[index]);
+}
+
+let car = {
+    type: 'Fiat',
+    model: '500',
+    color: 'white',
+  };
+  
+  for (let index in car) {
+    console.log(index, car[index]);
+  }
+
+  let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+
+  for (let index in names){
+      console.log('Olá ' + names[index]);
+  }
+
+  let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+  for (let key in car){
+      console.log(key, car[key]);
+  }
